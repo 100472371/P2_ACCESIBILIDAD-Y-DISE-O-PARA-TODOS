a9585ce142d724c3d69ab19935e82635b3157247
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-23 10:41:19 +0000

    Primer commit

---
+++ lib/index.dart
@@ -0,0 +1,4 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/pages/profile_page/profile_page_widget.dart' show ProfilePageWidget;
+export '/pages/sign_in_up/sign_in_up_widget.dart' show SignInUpWidget;
